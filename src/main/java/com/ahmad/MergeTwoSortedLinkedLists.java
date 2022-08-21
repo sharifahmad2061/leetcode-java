42d9c5c854d8ce01422551f75fcd94f8f094230b
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-21 22:38:05 +0700

    completes new implementation

--- src/main/java/com/ahmad/MergeTwoSortedLinkedLists.java
+++ src/main/java/com/ahmad/MergeTwoSortedLinkedLists.java
@@ -28,6 +28,37 @@ public class MergeTwoSortedLinkedLists {
         else
             return list1;
     }
+
+    public ListNode mergeTwoLists2(ListNode list1, ListNode list2) {
+        if (list1 == null && list2 == null)
+            return null;
+        else if (list1 == null)
+            return list2;
+        else if (list2 == null)
+            return list1;
+        else {
+            ListNode res = null;
+            if (list1.val <= list2.val) {
+                res = new ListNode(list1.val, list2);
+                list1 = list1.next;
+            } else {
+                res = new ListNode(list2.val, list1);
+                list2 = list2.next;
+            }
+            while (list1 != null && list2 != null) {
+                if (list1.val <= list2.val) {
+                    ListNode temp = new ListNode(list1.next.val, list1.next.next);
+                    list1.next = list2;
+                    list1 = temp;
+                } else {
+                    ListNode temp = new ListNode(list2.next.val, list2.next.next);
+                    list2.next = list1;
+                    list2 = temp;
+                }
+            }
+            return res;
+        }
+    }
 }
 
 // 1 2 4 list1
