f8c270ef704b5ae5fbcb9f96ec0434c856f5966d
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-21 16:20:02 +0700

    initialize project

---
+++ src/main/java/com/ahmad/App.java
@@ -0,0 +1,13 @@
+package com.ahmad;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
