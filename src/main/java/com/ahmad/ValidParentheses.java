d2f15075e5584e7d7e4261fc64a97f382f08831c
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-21 16:45:27 +0700

    completes basic task

--- src/main/java/com/ahmad/ValidParentheses.java
+++ src/main/java/com/ahmad/ValidParentheses.java
@@ -1,8 +1,36 @@
 package com.ahmad;
 
-public class ValidParentheses {
-    public boolean isValid(String s) {
+import java.util.Stack;
 
-        return false;
+public class ValidParentheses {
+    private char opposite(char c) {
+        if (c == ')')
+            return '(';
+        else if (c == '}')
+            return '{';
+        else if (c == ']')
+            return '[';
+        else
+            return 'i';
+    }
+
+    public boolean isValid(String s) {
+        if (s.isEmpty())
+            return false;
+        Stack<Character> brackets = new Stack<Character>();
+        for (char bracket : s.toCharArray()) {
+            if (bracket == ')' || bracket == '}' || bracket == ']') {
+                if (brackets.isEmpty() || brackets.peek() != opposite(bracket))
+                    return false;
+                else
+                    brackets.pop();
+            } else if (bracket == '(' || bracket == '{' || bracket == '[') {
+                brackets.push(bracket);
+            } else
+                return false; // invalid character
+        }
+        if (!brackets.isEmpty())
+            return false; // if an opening bracket is last parenthesis
+        return true;
     }
 }
--- src/test/java/com/ahmad/ValidParenthesesTest.java
+++ src/test/java/com/ahmad/ValidParenthesesTest.java
@@ -1,10 +1,22 @@
 package com.ahmad;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class ValidParenthesesTest {
+    ValidParentheses validParentheses = new ValidParentheses();
+
     @Test
-    void invalidParenthesisReturnFalse(){
-        Boolean actualResult = ValidParentheses.
+    void invalidParenthesisReturnFalse() {
+        final Boolean actualResult = validParentheses.isValid("[)");
+        final Boolean expectedResult = false;
+        Assertions.assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void validParenthesisReturnTrue() {
+        boolean actualResult = validParentheses.isValid("(){}[]");
+        boolean expectedResult = true;
+        Assertions.assertEquals(expectedResult, actualResult);
     }
 }
