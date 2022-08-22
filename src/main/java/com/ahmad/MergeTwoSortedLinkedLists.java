794bcba243168495aa3649fa63049b3c3b81769d
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-22 09:38:26 +0700

    adds a third solution

--- src/main/java/com/ahmad/MergeTwoSortedLinkedLists.java
+++ src/main/java/com/ahmad/MergeTwoSortedLinkedLists.java
@@ -72,6 +72,29 @@ public class MergeTwoSortedLinkedLists {
             return res;
         }
     }
+
+    public ListNode mergeTwoLists3(ListNode list1, ListNode list2) {
+        ListNode head = new ListNode(0);
+        ListNode handler = head;
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                handler.next = list1;
+                list1 = list1.next;
+            } else {
+                handler.next = list2;
+                list2 = list2.next;
+            }
+            handler = handler.next;
+        }
+
+        if (list1 != null) {
+            handler.next = list1;
+        } else if (list2 != null) {
+            handler.next = list2;
+        }
+
+        return head.next;
+    }
 }
 
 // 1 2 4 list1
