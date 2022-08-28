b02cfe2809496297e9ae4198ca43b820fd2e6a71
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-28 11:20:38 +0700

    solution works but timelimit exceeds on large input

--- src/main/java/com/ahmad/RemovingStarsFromAString.java
+++ src/main/java/com/ahmad/RemovingStarsFromAString.java
@@ -15,21 +15,32 @@ public class RemovingStarsFromAString {
         // else input[i] = '#';
         // }
         // }
-        String res = s;
-        if (s.contains("*")) {
+        // String res = s;
+        // if (s.contains("*")) {
+        // int index = s.indexOf("*");
+        // if (index != -1) {
+        // for (int i = index; i < s.length(); i++) {
+        // if (res.charAt(i) == '*') {
+        // if (i - 1 > 0) {
+        // res = res.substring(0, i - 1) + s.substring(i + 1);
+        // } else {
+        // res = res.substring(i + 1);
+        // }
+        // }
+        // }
+        // }
+        // }
+        // return res;
+
+        if (!s.contains("*"))
+            return s;
+        else {
             int index = s.indexOf("*");
-            if (index != -1) {
-                for (int i = index; i < s.length(); i++) {
-                    if (res.charAt(i) == '*') {
-                        if (i - 1 > 0) {
-                            res = res.substring(0, i - 1) + s.substring(i + 1);
-                        } else {
-                            res = res.substring(i + 1);
-                        }
-                    }
-                }
+            if (index - 1 > 0) {
+                return removeStars(s.substring(0, index - 1) + s.substring(index + 1));
+            } else {
+                return removeStars(s.substring(index + 1));
             }
         }
-        return res;
     }
 }
