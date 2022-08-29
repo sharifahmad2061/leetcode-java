74c83ff34d0dab968e05dd9eafcb5fb4597c5c68
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-29 13:41:43 +0700

    changed to iterative approach

--- src/main/java/com/ahmad/FibonacciNumbers.java
+++ src/main/java/com/ahmad/FibonacciNumbers.java
@@ -1,23 +1,13 @@
 package com.ahmad;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class FibonacciNumbers {
-    Map<Integer, Integer> fibonacciMapping = new HashMap<>();
-
     public int fib(int n) {
-        int returnValue = 0;
-        if (fibonacciMapping.containsKey(n))
-            returnValue = fibonacciMapping.get(n);
-        if (n == 1)
-            returnValue = 1;
-        else if (n == 0)
-            returnValue = 0;
-        else {
-            returnValue = fib(n - 1) + fib(n - 2);
+        if (n <= 1)
+            return n;
+        int[] fibArray = new int[n + 1];
+        for (int i = 2; i <= n; i++) {
+            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
         }
-        fibonacciMapping.put(n, returnValue);
-        return returnValue;
+        return fibArray[n];
     }
 }
