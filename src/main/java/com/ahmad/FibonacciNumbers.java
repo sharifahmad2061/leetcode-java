42dcf16ebecf8b6ee3d1ae9758003167db86d769
Ahmad, Sharif (Agoda) <sharif.ahmad@agoda.com>
2022-08-29 13:42:57 +0700

    adds inital value to fibArray

--- src/main/java/com/ahmad/FibonacciNumbers.java
+++ src/main/java/com/ahmad/FibonacciNumbers.java
@@ -5,6 +5,8 @@ public class FibonacciNumbers {
         if (n <= 1)
             return n;
         int[] fibArray = new int[n + 1];
+        fibArray[0] = 0;
+        fibArray[1] = 1;
         for (int i = 2; i <= n; i++) {
             fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
         }
